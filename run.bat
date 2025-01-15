2d167078c8c9fe65605278919201e8a57534cfbe
lucaspuglielli <lucaspuglielli@gmail.com>
2025-01-14 23:38:11 -0300

    Limitando interface do usuário para apenas o jupyter notebook

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ python -m venv .venv
 call .venv\Scripts\activate
 pip install -r requirements.txt
 docker-compose up -d
-@REM jupyter notebook "main.ipynb"
-jupyter-lab "main.ipynb"
+jupyter notebook "main.ipynb"
+@REM jupyter-lab "main.ipynb"
 docker-compose down
 deactivate
